f80d1895032c776ed0e4b22422e6e60c1d9e3615
Dogeon188 <dogcatdog188@gmail.com>
2020-11-24 22:49:12 +0800

    modal in index.html

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-/// <reference path="./lib/sweetalert2.all.min.js" />
-
-var coupon_list = [{
-    "id": "ck210",
+var stand_list = [{
     "title": "210角落廢物",
     "desc": "爆米花&氣泡飲",
-    "image": "ck210.png"
+    "image": "ck210.png",
+    "html": `
+    酥脆爆米花搭配順口氣泡飲～
+    掰不下去了～
+    `
 }, {
-    "id": "ckmc",
     "title": "建中軍武社",
     "desc": "實彈射擊體驗",
-    "image": "ckmc.png"
+    "image": "ckmc.png",
+    "html": ``
 }, {
-    "id": "test",
     "title": "徵的就是你",
     "desc": "加入十班聯盟湊齊十個攤位吧",
-    "image": "test/1.png"
+    "image": "test/1.png",
+    "html": `詳情請洽<strong>210黃庭耀</strong>或<strong>210毛柏毅</strong>`
 }];
 
 $(function () {
-    coupon_list.forEach(function (now, id) {
+    stand_list.forEach(function (now, id) {
         $("main").append(`
-        <div class="link" id="link-${now.id}">
+        <div class="link" id="link-${id}">
             <img src="img/${now.image}" alt="${now.title}">
-            <div>${now.title}<p>${now.desc}</p></div>
+            <div><b>${now.title}</b><p>${now.desc}</p></div>
         </div>`);
+        $("body").on("click", `#link-${id}`, function() {
+            Swal.fire({
+                grow: "fullscreen",
+                imageUrl: `img/${now.image}`,
+                imageAlt: now.title,
+                title: `<strong>${now.title}</strong>`,
+                html: now.html,
+                showCloseButton: true,
+                showConfirmButton: false
+            });
+        });
     });
 });
-
-
-
-$("body")
-.on("click", "#link-ck210", function () {$("#modal-ck210").show();})
-.on("click", "#close-ck210", function () {$("#modal-ck210").hide();});
