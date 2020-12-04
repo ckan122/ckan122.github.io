ff7949d07861ecd1d188fdfa298c8dbdf7c9f33d
Dogeon188 <dogcatdog188@gmail.com>
2020-12-04 15:40:09 +0800

    many changes owo

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-var stand_list = [{
-    "title": "210 角落廢物",
-    "desc": "酥脆爆米花 & 清爽氣泡飲",
-    "image": "ck210.png",
-    "html": `
-    <a href="doc/210.pdf"><img src="img/ck210_desc.png" style="max-height: 80vh; max-width: 45vw;"></a><br>
-    點擊圖片可查看大圖PDF
-    `
-}, {
-    "title": "建中軍武社",
-    "desc": "實彈射擊體驗",
-    "image": "ckmc.png",
-    "html": `
-    有機會獲得精美好禮OWO
-    `
-}];
+var stand_list = null;
 
 $(function () {
-    stand_list.forEach(function (now, id) {
+    [{
+        "title": "210 角落廢物",
+        "desc": "酥脆爆米花 & 清爽氣泡飲",
+        "image": "ck210.png",
+        "html": `
+        <a href="doc/210.pdf"><img src="img/ck210_desc.png" style="max-height: 80vh; max-width: 45vw;"></a><br>
+        點擊圖片可查看大圖PDF
+        `
+    }, {
+        "title": "建中軍武社",
+        "desc": "實彈射擊體驗",
+        "image": "ckmc.png",
+        "html": `
+        有機會獲得精美好禮OWO
+        `
+    }].forEach(function (now, id) {
         $("main").append(`
         <div class="link" id="link-${id}">
             <img src="img/${now.image}" alt="${now.title}">
@@ -34,4 +34,9 @@ $(function () {
             });
         });
     });
+    $("main").append(`
+    <a href="doc/校慶會場平面圖.jpg" class="link" style="display: block;">
+        <img src="img/map.png" alt="校慶平面圖">
+        <div><b>校慶平面圖</b></div>
+    </a>`);
 });
